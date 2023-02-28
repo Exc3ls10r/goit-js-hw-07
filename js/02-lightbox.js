1447549354bdb4dca2faeb1109e5faf53b8c2c6f
Exc3ls10r <111652846+Exc3ls10r@users.noreply.github.com>
2023-02-28 13:51:28 +0200

    task-02-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector('.gallery');
+
+function createGallery(items) {
+  return items.map( ({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  }).join("");
+};    
+
+galleryContainerRef.innerHTML = createGallery(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
