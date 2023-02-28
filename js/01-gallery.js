e73d88c33ccfb381f2265e6060abed8ca749c808
Exc3ls10r <111652846+Exc3ls10r@users.noreply.github.com>
2023-02-28 02:22:34 +0200

    task-01-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector('.gallery');
+
+function createGallery(items) {
+  return items.map( ({ preview, original, description }) => {
+    return `<div class="gallery__item"><a class="gallery__link" href="${original}">
+<img class="gallery__image" src="${preview}" data-source="${original}"
+alt="${description}" /></a></div>`
+  }).join("");
+};
+
+galleryContainerRef.innerHTML = createGallery(galleryItems);
+galleryContainerRef.addEventListener('click', onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return
+  }
+
+  const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" width="800" height="600">`)
+
+  instance.show()
+
+  galleryContainerRef.addEventListener('keydown', onEscClose);
+
+  function onEscClose(env) {
+    if (env.code === "Escape") {
+      instance.close()
+      galleryContainerRef.removeEventListener('keydown', onEscClose);
+    }
+  }
+}
