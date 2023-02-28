1eccd5979b41d0fb238755cbefa8b0d38909c639
Exc3ls10r <111652846+Exc3ls10r@users.noreply.github.com>
2023-02-28 13:54:38 +0200

    task-02-done-correction

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,7 +7,7 @@ function createGallery(items) {
   return items.map( ({ preview, original, description }) => {
     return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`
+</a></li>`
   }).join("");
 };    
 
